e137eb56497de2e41915433aafac573b85172b86
Tabee <ayvanika@gmail.com>
2022-11-07 23:21:40 +0200

    simple gravity before i fall fkn asleep

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public int gravityValue = -10;
+
+    void Update()
+    {
+        Physics.gravity = new Vector3(0, gravityValue, 0);
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            gravityValue++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            gravityValue--;
+        }
+    }
+}
