c4f4b983ecaca7d06a0a3e672d61eaab0850a55e
Tabee <ayvanika@gmail.com>
2022-11-13 16:10:11 +0200

    size machine

--- Assets/Scripts/SizeChanger.cs
+++ Assets/Scripts/SizeChanger.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SizeChanger : MonoBehaviour
 {
+    public GameObject triggeredObject;
+    public Transform player;
+    public Transform playerBody;
+    public TMP_Text scaleText;
+
     public bool playerNearby;
+
+    private Vector3 scaleChange;
+    private void Awake()
+    {
+        scaleChange = new Vector3(0.2f, 0.2f, 0.2f);
+    }
     void Update()
     {
+        if (playerNearby == true && Input.GetKeyDown(KeyCode.E))
+        {
+            if (triggeredObject.gameObject.name == "sizeUp")
+            {
+                triggeredObject.GetComponent<Renderer>().material.color = Color.red;
+                player.transform.localScale += scaleChange;
+                playerBody.transform.position = new Vector3(-8.3813f, 0.8991978f, -3.086281f);
+                Invoke("ResetColor", 0.5f);
+            }
+            else if (triggeredObject.gameObject.name == "sizeDown")
+            {
+                triggeredObject.GetComponent<Renderer>().material.color = Color.red;
+                player.transform.localScale -= scaleChange;
+                playerBody.transform.position = new Vector3(-8.3813f, 0.8991978f, -0.05828547f);
 
+                Invoke("ResetColor", 0.5f);
+            }
+        }
+        scaleText.text = "Scale: \n" + player.transform.localScale;
+    }
+    public void ResetColor()
+    {
+        triggeredObject.GetComponent<Renderer>().material.color = Color.cyan;
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -23,5 +57,4 @@ public class SizeChanger : MonoBehaviour
             playerNearby = false;
         }
     }
-
 }
